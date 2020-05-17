fce48f11738588fd58c74ee42909b72c03ac8e5d
numazaki345 <ryo.091979@gmail.com>
2020-05-17 15:25:16 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
     # 下記はサーバーサイド実装の記述(確認の為一時的にコメントアウトします)←root_pathにかけたくないので最後に消去します！（代役は記載しました！）
     # before_action :authenticate_user!
     # ログインしていないとルートに飛ぶ
+    before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
     # 登録時のストロングパラメータ追加
     protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname,:firstname,:lastname_kana,:firstname_kana,:birthyear_id,:birthmonth_id,:birthday_id,:zipcode,:pref_id,:city,:address,:buildingname,:phone])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     end
   
     def set_card
